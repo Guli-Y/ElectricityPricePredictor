30760cd5e8ac98b8e931c5be1d54c77ce7f5a76c
Guli <g.yimingjiang@gmail.com>
2020-11-26 00:06:51 +0100

    till hyperparams of sarima

--- electricity_price_predictor/data_2.py
+++ electricity_price_predictor/data_2.py
@@ -21,30 +21,29 @@ def get_price(path='../raw_data/price/'):
     df = df[df.price!='-'] # filtering the timestamps till 24.11.2020
     df['time'] = pd.to_datetime(df['time'], format='%d.%m.%Y %H:%M')
     df['price'] = df.price.astype('float')
+    df.set_index(pd.DatetimeIndex(df['time']), inplace=True)
+    df.drop(columns=['time'], inplace=True)
     return df
 
 def get_shifted_price():
     df = get_price()
-    df_1 = df.iloc[:2090, :]
-    df_2 = df.iloc[2090:7132, :]
-    df_3 = df.iloc[7132:10827, :]
-    df_4 = df.iloc[10827:16037, :]
-    df_5 = df.iloc[16037:19564, :]
-    df_6 = df.iloc[19564:24774, :]
-    df_7 = df.iloc[24774:28301, :]
-    df_8 = df.iloc[28301:33511, :]
-    df_9 = df.iloc[33511:37206, :]
-    df_10 = df.iloc[37206:42248, :]
-    df_11 = df.iloc[42248:45943, :]
-    df_12 = df.iloc[45943:,]
-    no_shift = [df_1, df_3, df_5, df_7, df_9, df_11]
-    shift_list = [df_2, df_4,df_6, df_8, df_10, df_12]
-    price_df = df_1.set_index('time')
-    for data in no_shift[1:]:
-        data.set_index('time', inplace=True)
-        price_df = pd.concat([price_df, data])
-    for data in shift_list:
-        data.set_index('time', inplace=True)
+    df_1 = df.loc['2015-01-01 00:00:00':'2015-03-29 01:00:00']
+    df_2 = df.loc['2015-03-29 02:00:00':'2015-10-25 02:00:00']
+    df_3 = df.loc['2015-10-25 03:00:00':'2016-03-27 01:00:00']
+    df_4 = df.loc['2016-03-27 02:00:00':'2016-10-30 02:00:00']
+    df_5 = df.loc['2016-10-30 03:00:00':'2017-03-26 01:00:00']
+    df_6 = df.loc['2017-03-26 02:00:00':'2017-10-29 02:00:00']
+    df_7 = df.loc['2017-10-29 03:00:00':'2018-03-25 01:00:00']
+    df_8 = df.loc['2018-03-25 02:00:00':'2018-10-28 02:00:00']
+    df_9 = df.loc['2018-10-28 03:00:00':'2019-03-31 01:00:00']
+    df_10 = df.loc['2019-03-31 02:00:00':'2019-10-27 02:00:00']
+    df_11 = df.loc['2019-10-27 03:00:00':'2020-03-29 01:00:00']
+    df_12 = df.loc['2020-03-29 02:00:00':'2020-10-25 02:00:00']
+    df_13 = df.loc['2020-10-25 03:00:00':'2020-11-24 23:00:00']
+    df_list = [df_1, df_2, df_3, df_4, df_5, df_6, df_7,
+                df_8, df_9, df_10, df_11, df_12, df_13]
+    price_df = df_1
+    for data in df_list[1:]:
         data = data.shift(periods=-1).dropna()
         price_df = pd.concat([price_df, data])
     return price_df
