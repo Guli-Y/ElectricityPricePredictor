010da5d6552b895e7d53b929349b22b047e3387c
ChikeMartin <cezeadebor@yahoo.com>
2020-11-29 15:34:31 +0100

    ...

--- setup.py
+++ setup.py
@@ -5,22 +5,12 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-# setup(name='electricity_price_predictor',
-#       version="1.0",
-#       description="Project Description",
-#       packages=find_packages(),
-#       test_suite = 'tests',
-#       # include_package_data: to install data from MANIFEST.in
-#       include_package_data=True,
-#       scripts=['scripts/electricity_price_predictor-run'],
-#       zip_safe=False)
-
-
-setup(name='FlaskAPI',
+setup(name='electricity_price_predictor',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
       test_suite = 'tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      description='flask api')
+      scripts=['scripts/electricity_price_predictor-run'],
+      zip_safe=False)
