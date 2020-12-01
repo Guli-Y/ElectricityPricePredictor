78152af42b3d8bd4b4b0edded86e2c723408da24
griseldo <g.bajraktari@outlook.com>
2020-12-01 15:11:16 +0100

    changed the app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ st.markdown("# ML Project - Electricity price predictor")
 # img = mpimg.imread('/notebooks/DK1.png')
 # # plt.imshow(img)
 
-image = Image.open('raw_data/test.png')
+image = Image.open('test.png')
 st.image(image, use_column_width=True)
 
 
