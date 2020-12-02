a4ebb665e03cb51851e7482a532df4a3f4806c46
griseldo <g.bajraktari@outlook.com>
2020-12-02 12:16:16 +0100

    changed app.py

--- app.py
+++ app.py
@@ -23,12 +23,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+import base64
 # import matplotlib.pyplot as plt
 # import matplotlib.image as mpimg
 # from electricity_price_predictor.data import get_all, get_price
 # from sklearn.preprocessing import MinMaxScaler
 # from sklearn.linear_model import LinearRegression
 # import statsmodels.formula.api as sm
+
+
+
+
+st.set_page_config(
+    page_title="electricity price prediction",
+    page_icon=":zap:",
+    layout="centered", # wide
+    initial_sidebar_state="auto") # collapsed
+
 
 raiden = Image.open('raiden.jpg')
 st.sidebar.markdown(f"""
@@ -39,7 +50,7 @@ st.sidebar.markdown(f"""
 
 SIDEBAR_CSS = f"""
 <h1 style=“font-size:44px;
-color:red“>
+color:blue>
 """
 
 ### Creating the sidebar
@@ -61,6 +72,7 @@ st.markdown('<style>h1{color: blue;}</style>', unsafe_allow_html=True)
 
 
 
+
 ### Table 1, price prediction
 st.markdown("Table 1 - DK1 4th of December - Electricity Prices")
 @st.cache
@@ -74,12 +86,7 @@ def get_dataframe_data():
 
 df = get_dataframe_data()
 
-st.write(df)
-
-###
-
-st.markdown("Line Chart")
-
+### Line chart function
 @st.cache
 def get_line_chart_data():
     print('get_line_chart_data called')
@@ -87,10 +94,20 @@ def get_line_chart_data():
             np.random.randn(20, 3),
             columns=['a', 'b', 'c']
         )
+###
 
-# df = get_line_chart_data()
 
-st.line_chart(df)
+#### Ploting the table and chart side by side
+col1, col2 = st.beta_columns([1,4])
+
+with col1:
+    st.write(df, use_column_width=True)
+with col2:
+    st.line_chart(df)
+
+###
+
+
 
 ### Table 2, price prediction
 st.markdown("Table 2 - 5th of December, Electricity Prices")
@@ -98,17 +115,52 @@ st.markdown("Table 2 - 5th of December, Electricity Prices")
 def get_dataframe_data():
     print('get_dataframe_data called')
     return pd.DataFrame(
-            np.random.randn(20, 2),
-            columns=('col %d' % i for i in range(2))
+            np.random.randn(24, 1),
+            columns=('col %d' % i for i in range(1))
         )
 
 df1 = get_dataframe_data()
 
-st.dataframe(df1.head().style.highlight_max(axis=0))
+# st.dataframe(df1.head().style.highlight_max(axis=0))
 ###
 
-st.markdown("""[links](https://pricepred-g.herokuapp.com/)
+
+#### Ploting the table and chart side by side
+col1, col2 = st.beta_columns([1,4])
+
+with col1:
+    st.write(df1, use_column_width=True)
+with col2:
+    st.line_chart(df)
+
+###
+
+
+
+### Creating a link to download the data or plot
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(
+        csv.encode()
+    ).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+
+st.markdown("""Click the link below to download the data in csv format""")
+
+st.markdown(
+    get_table_download_link(df), unsafe_allow_html=True
+    )
+
+
+st.markdown("""
+    Click the [link](https://pricepred-g.herokuapp.com/) to check the price prediction on the web app
     """)
+###
 
 
 
@@ -138,24 +190,10 @@ st.markdown("""[links](https://pricepred-g.herokuapp.com/)
 # st.write(prediction)
 
 
-
-
-
-
-
-
 # if __name__ == "__main__":
 #     #df = read_data()
 #     main()
 
-
-
-
-
-
-
-# img = mpimg.imread('/notebooks/DK1.png')
-# # plt.imshow(img)
 
 image = Image.open('test.png')
 
@@ -179,12 +217,7 @@ st.image(image, use_column_width=True)
 
 
 
-col1, col2 = st.beta_columns([1,4])
 
-with col1:
-    st.write(df, use_column_width=True)
-with col2:
-    st.line_chart(df)
 
 
 
