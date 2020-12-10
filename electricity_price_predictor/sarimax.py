9254b607b11b08fa174b20465b41a43ae14ad4e1
Guli <g.yimingjiang@gmail.com>
2020-12-10 14:25:31 +0100

    changed sarimax_forecast_24 so that it calls get_data only one time and reduces the the number of times that weather API being called

--- electricity_price_predictor/sarimax.py
+++ electricity_price_predictor/sarimax.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-def plot_forecast(forecast, train, lower, upper):
+def plot_forecast(forecast, past):
     '''it will plot a forecast'''
     fig, ax = plt.subplots(figsize=(10,4), dpi=100)
-    ax.plot(train, label='past', color='black')
-    ax.plot(forecast, label='forecast', color='blue')
-    ax.fill_between(lower.index, lower, upper, label='confidence interval',
+    ax.plot(past, label='past', color='black')
+    ax.plot(forecast.price, label='forecast', color='blue')
+    ax.fill_between(forecast.index, forecast.lower, forecast.upper, label='confidence interval',
                     color='k', alpha=.15)
     title = 'Electricity Price Forecast - next 48 hours (EUR/Mwh)'
     ax.set_title(title)
@@ -19,24 +19,26 @@ def plot_forecast(forecast, train, lower, upper):
     ax.grid(True)
     ax.format_xdata = mdates.DateFormatter('%d-%H-%m')
     fig.autofmt_xdate()
+    # save the forecast plot for heroku webpage
+    print('############## saving forecast plot ##############')
     plt.savefig('../forecast_data/forecast.png')
 
 
-def sarimax_forecast(hour=11):
-    '''hour: hour of a day, range(0, 23),
-    returns forecast, upper_intervals, lower_intervals, mape, mase, test, train'''
-
-    df_all = get_data(hour=hour)
+def sarimax_forecast(df):
+    '''it takes a dataframe with past and future values and split it into
+    train/forecast sets based on the availability of price and returns
+    forecast dataframe and past prices'''
 
     # split past and furture
-    past = df_all[~df_all.price.isnull()]
-    future = df_all[df_all.price.isnull()].drop('price', axis=1)
-
+    past = df[~df.price.isnull()]
+    future = df[df.price.isnull()].drop('price', axis=1)
+    # forecast for next time point only
     future = future.iloc[:1,:]
-    if future.temp.isnull()[0]:
+    if future.temp.isnull()[0]: # when weather forecast data is not available for that hour
         forecast = np.array([np.nan])
-        confidence_int =pd.DataFrame({'lower price':np.nan, 'upper price':np.nan}, index=['x'])
-
+        lower = np.array([np.nan])
+        upper = np.array([np.nan])
+        print('weather data is not available')
     else:
         past.index = pd.DatetimeIndex(past.index.values,
                                         freq=past.index.inferred_freq)
@@ -47,10 +49,10 @@ def sarimax_forecast(hour=11):
         # forecasting
         results = sarima.get_forecast(1, exog=future, alpha=0.05)
         forecast = sarima.forecast(1, exog=future, alpha=0.05)
-        confidence_int = results.conf_int()
+        lower = results.conf_int()['lower price'][0]
+        upper = results.conf_int()['upper price'][0]
+
     # create forecast df with datetimeIndex
-    lower = confidence_int['lower price'][0]
-    upper = confidence_int['upper price'][0]
     forecast = pd.DataFrame(dict(price=forecast, lower=lower, upper=upper),
                             index=future.index)
     past = past.iloc[-1:,0]
@@ -59,30 +61,34 @@ def sarimax_forecast(hour=11):
 def sarimax_forecast_24():
     '''it calls sarimax_forecast function and 24 times to get hourly forecast and
     plot the forecast results using plot_forecast function'''
+    df = get_data()
     forecasts = []
     pasts = []
+    # loop over to get forecast for each hour
     for i in range(1, 24):
-        forecast_i, past_i = sarimax_forecast(hour=i)
+        print(f"############## forecasting for {str(i)}:00 o'clock ##############")
+        df_i = df[df.index.hour==i]
+        forecast_i, past_i = sarimax_forecast(df_i)
         forecasts.append(forecast_i)
         pasts.append(past_i)
     # merge 24 hours
+    print('############## Merging forecasts data ##############')
     forecast_df, past_df = sarimax_forecast(hour=0)
     for forecast, past in zip(forecasts, pasts):
         forecast_df = pd.concat([forecast_df, forecast])
         past_df = pd.concat([past_df, past])
+    # sort the index
     forecast_df = forecast_df.sort_index()
     past_df = past_df.sort_index()
     return forecast_df, past_df
-
-def plot_sarimax_forecast(hour=11):
-    '''it calls sarimax_forecast function and
-    plot the forecast results using plot_forecast function'''
-    forecast, past = sarimax_forecast(hour=hour)
-    plot_forecast(forecast.price, past, forecast.lower, forecast.upper)
 
 def plot_sarimax_forecast_24():
     '''it calls sarimax_forecast_24 function and
     plot the forecast results using plot_forecast function'''
     forecast, past = sarimax_forecast_24()
+    # save the forecast results
+    print('############## saving forecast results ##############')
     forecast.to_csv('../forecast_data/forecast_data.csv')
-    plot_forecast(forecast.price, past, forecast.lower, forecast.upper)
+    # plot the forecast results
+    print('############## plotting forecast results ##############')
+    plot_forecast(forecast, past)
