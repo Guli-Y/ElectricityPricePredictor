424da1d24183ffafe773aca0ec7c5e332e769f74
Guli <g.yimingjiang@gmail.com>
2020-12-11 13:00:00 +0100

    corrected install requirements

--- setup.py
+++ setup.py
@@ -6,15 +6,12 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 
-REQUIRED_PACKAGES = [
-    'joblib']
-
 setup(name='electricity_price_predictor',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
       test_suite = 'tests',
-      install_requires=REQUIRED_PACKAGES,
+      install_requires=requirements,
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       scripts=['scripts/electricity_price_predictor-run'],
