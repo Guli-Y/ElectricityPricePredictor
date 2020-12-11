a4c314baf42ecf781a07ff96f0d204003cf73e10
Guli <g.yimingjiang@gmail.com>
2020-12-11 13:00:34 +0100

    added proper test

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,10 +1,19 @@
 # -*- coding: UTF-8 -*-
 
-# Import from standard library
-from datetime import timedelta, date
-from electricity_price_predictor.data import get_updated_price
+import os
+import electricity_price_predictor
+import pandas as pd
+from electricity_price_predictor.data import get_shifted_price
+from datetime import date
 import pytest
 
 
-def test_get_updated_price():
-    assert get_updated_price().index[-1].date() == date.today()+timedelta(days=1)
+def test_get_shifted_price():
+    path = os.path.dirname(os.path.abspath(__file__))
+    file = os.path.join(path, '..', 'data','updated_price.csv')
+    df = pd.read_csv(file, parse_dates=True, index_col='time')
+    today = date.today()
+    assert df.index.date()[-1] >= today
+    assert df.price.isnull().sum()[0] == 6
+    df_shifted = get_shifted_price(df)
+    assert df_shifted.price.isnull().sum()[0] == 0
