1c517482b61d96537e3aa66d3d55fc71d810a8ae
Guli <g.yimingjiang@gmail.com>
2020-12-27 17:10:14 +0100

    updated the test for get_shifted_price

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,16 +1,14 @@
 # -*- coding: UTF-8 -*-
 
 import os
-import electricity_price_predictor
 import pandas as pd
 from electricity_price_predictor.data import get_shifted_price
 from datetime import date
-import pytest
 
 
 def test_get_shifted_price():
     path = os.path.dirname(os.path.abspath(__file__))
-    file = os.path.join(path, '..', 'data','updated_price.csv')
+    file = os.path.join(path, '..', 'electricity_price_predictor','data', 'updated_price.csv')
     df = pd.read_csv(file, parse_dates=True, index_col='time')
     today = date.today()
     assert df.index[-1] >= today
