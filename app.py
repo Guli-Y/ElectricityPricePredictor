28e437a417ba300efcd15f3fb7106e9b2d9e60a3
Guli <g.yimingjiang@gmail.com>
2021-01-03 23:46:30 +0100

    changed download filename

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
 from datetime import date, timedelta
 import base64
-
 
 st.set_page_config(page_title="electricity-price-predictor",
                     page_icon=":zap:",
@@ -64,7 +61,7 @@ st.markdown("Table 1 - DK1 " + twoday_ahead + " - Hourly Electricity Price")
 col1, col2 = st.beta_columns([5,1])
 
 with col1:
-    st.area_chart(df['price'])
+    st.line_chart(df['price'])
 
 with col2:
     st.write(df['price'], use_column_width=True)
@@ -84,6 +81,6 @@ def get_download_link(df):
     b64 = base64.b64encode(
         csv.encode()
     ).decode()  # some strings <-> bytes conversions necessary here
-    return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+    return f'<a href="data:file/csv;base64,{b64}" download="{today}.csv">Download csv file</a>'
 
 st.markdown(get_download_link(df), unsafe_allow_html=True)
