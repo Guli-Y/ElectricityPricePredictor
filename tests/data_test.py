ba4810a28990c7ffece197822549a95c1a02092c
gulinuer yimingjiang <g.yimingjiang@gmail.com>
2021-01-16 21:41:06 +0100

    changed the test

--- tests/data_test.py
+++ tests/data_test.py
@@ -7,8 +7,6 @@ from datetime import date
 def test_get_shifted_price():
     location = 'https://storage.googleapis.com/electricity_price_predictor/data/updated_price.csv'
     df = pd.read_csv(location, parse_dates=True, index_col='time')
-    today = date.today()
-    assert df.index[-1] >= today
     assert df.price.isnull().sum() == 6
     df_shifted = get_shifted_price(df=df)
     assert df_shifted.price.isnull().sum() == 0
